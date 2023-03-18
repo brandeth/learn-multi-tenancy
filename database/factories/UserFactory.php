0b774bc980687a915b68844ac0a06dbe233b139e
Rey Garcia <jreygarcia24@gmail.com>
2023-03-18 20:40:51 +0800

    Added Tenant model with migration and factory.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,9 +20,11 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'role' => 'admin',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('password'),
             'remember_token' => Str::random(10),
+            'tenant_id' => \App\Models\Tenant::factory()
         ];
     }
 
